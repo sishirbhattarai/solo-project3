2a853283b5342b63c95e5cafef7e121bd5ba01d1
Sishir <sishir.bhattarai@outlook.com>
2021-02-26 13:06:28 -0700

    connected backend with mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 const express = require("express");
-// const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
+connectDB();
+//const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
