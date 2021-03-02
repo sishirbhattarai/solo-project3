0e3c9f0fe8cadefdef056441847b85320ac14a2d
Sishir <sishir.bhattarai@outlook.com>
2021-03-01 20:57:12 -0700

    modified cart css

--- clients/src/screens/CartScreen.js
+++ clients/src/screens/CartScreen.js
@@ -10,6 +10,10 @@ const CartScreen = () => {
                 <h2>Shopping Cart</h2>
 
                 <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
             </div>
             <div className="cartscreen__right">
                 <div className="cartscreen__info">
