3bf3b04b6932511c0f272f214bcf0fcee9903ba4
Sishir <sishir.bhattarai@outlook.com>
2021-03-02 21:58:27 -0700

    added proxy server

--- clients/src/screens/HomeScreen.js
+++ clients/src/screens/HomeScreen.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+
 import "./HomeScreen.css";
-import Product from "../components/Product.js"
+import Product from "../components/Product.js";
+
+
+import { getProducts as listProducts } from "../redux/actions/productActions";
 
 const HomeScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, loading, error } = getProducts;
+
+    //everytime the page loads, the page will list the products
+    useEffect(() => {
+        dispatch(listProducts())}, [dispatch]);
+
+
     return (
         <div className="homescreen">
             <h2 className="homescreen__title">Recent Products</h2>
