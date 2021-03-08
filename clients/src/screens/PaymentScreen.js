c13db62e57ec24c92de4bef57f373dd4a7e8e837
Sishir <sishir.bhattarai@outlook.com>
2021-03-08 14:47:40 -0700

    edited payment form

--- clients/src/screens/PaymentScreen.js
+++ clients/src/screens/PaymentScreen.js
@@ -4,71 +4,35 @@ import "./PaymentScreen.css";
 
 function Payment() {
     return (
-<div className="row">
-  <div className="col-75">
-    <div className="container">
-      <form action="/action_page.php">
-
-        <div className="row">
-          <div className="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe"/>
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com"/>
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"/>
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York"/>
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY"/>
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001"/>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa"> </i>
-              <i class="fa fa-cc-amex"></i>
-              <i class="fa fa-cc-mastercard"></i>
-              <i class="fa fa-cc-discover"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe"/>
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"/>
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September"/>
-
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018"/>
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352"/>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <label>
-          {/* <input type="checkbox" checked="checked" name="sameadr">Shipping address same as billing</input> */}
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn"/>
-      </form>
+<div className="wrapper">
+  <h2>Payment Form</h2>
+  <form method="POST">
+    <h4>Account</h4>
+    <div className="input-group">
+      <div className="input-box">
+        <input type ="text"
+        placeholder="Full Name" required
+        className="name"/>
+        <i className="fa fa-user icon"></i>
+      </div>
+      <div className="input-box">
+        <input type="text"
+        placeholder="Nick Name" required
+        className="name"/>
+        <i className="fa fa-user icon"></i>
+      </div>
     </div>
-  </div>
+    <div className="input-group">
+      <div className="input-box">
+        <input type="email"
+        placeholder="Email address" required
+        className="name"/>
+        <i className="fa fa-envelope"></i>
+
+      </div>
+    </div>
+  </form>
+
   </div>
 
     )};
