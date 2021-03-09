61a855101bbb2969acf9eb40994362981afd6f5e
Sishir <sishir.bhattarai@outlook.com>
2021-03-08 17:35:49 -0700

    added products

--- backend/data/products.js
+++ backend/data/products.js
@@ -18,59 +18,60 @@ const products = [
     countInStock: 15,
   },
   {
-    name: "PlayStation 5",
+    name: "The Everyday Headphones",
     imageUrl:
-      "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkldtzZWFSfT83xs_yb3wpPlcx98fvDiGNeoV3igUZFN8-l9ROFEB0nrfXSddaXJ3I0m1EeSw&usqp=CAc",
     description:
-      "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+      "With booming sound and superior noise cancellation, you can block out all distractions and stay focused on your mission. Quality sound, Quality Music",
     price: 499,
     countInStock: 15,
   },
   {
-    name: "Iphone 12",
+    name: "SB-2000 Pro Subwoofer",
     imageUrl:
-      "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_PffGxG57mAoSiNGhYlh31DCGNsuwpONE6-nS2UZfOxEU6u1rsIBClWzQKnBjXN4RBOUuqek&usqp=CAc",
     description:
-      "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-    price: 1099,
+      "aw-dropping deep bass extension and massive output with stunning musicality that will delight both audiophiles and home theater fans. Features all-new 12-inch SVS driver, 550 Watts RMS, 1,500+ Watts peak power Sledge amplifier with fully discrete MOSFET output, and SVS subwoofer DSP smartphone app in a compact cabinet measuring roughly 15-inches on all sides.",
+    price: 899,
     countInStock: 10,
   },
   {
-    name: "Cannon EOS-1D",
+    name: "Beats Headphone",
     imageUrl:
-      "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiDSh9i4RneRiK-oCI-Ims1GDcQnOy3AC3MLAkAfjxdSYRUxhKeyHUSfNZj99WNLqwNWfK0HDF&usqp=CAc",
     description:
-      "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
-    price: 1300,
-    countInStock: 5,
-  },
-  {
-    name: "Amazon Alexa",
-    imageUrl:
-      "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-    description:
-      "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
-    price: 50,
-    countInStock: 25,
-  },
-  {
-    name: "Audio Technica Headphones",
-    imageUrl:
-      "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    description:
-      "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
-    price: 233,
+      "Beats Studio3 Wireless Noise Cancelling Headphones with Apple W1 Headphone Chip - Red. Beats Products · Always-ready wireless earphones · High‑performance wireless earphones · Everyday wireless on‑ear headphones.",
+    price: 633,
     countInStock: 4,
   },
   {
-    name: "JBL FLIP 4",
+    name: "JBL TUNE 700BT",
     imageUrl:
-      "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv64UmgucAr1JfjGTxvQ3WVI6qDFbmxw6osR6OVXudXatUcS4Y_5NEeQhgnBeNrecAIxipogg&usqp=CAc",
     description:
-      "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
-    price: 140,
+      "For over 70 years, JBL has engineered the precise, impressive sound found in big venues around the world. These headphones reproduce that same JBL sound, punching out bass that’s both deep and powerful.",
+    price: 340,
     countInStock: 10,
   },
+  {
+    name: "MacBook Pro 15 Retina ",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQkXgbIqOx2JMDzoLJDz0toLQ5qSUCg3tm1KiYljvxdJv-tvIY3CFanDDpcNlb4kNkcyEcMNY&usqp=CAc",
+    description:
+      "MacBook Pro 15 Retina | Certified Refurbished | Core i7 2.2GHz | 16GB RAM | SSD. An incredibly sharp retina display is included with the MacBook Pro 15.4-inch Laptop. With millions of pixels and an incredible resolution, you'll enjoy reading crystal-clear text and experiencing vibrant, realistic photos, video, and other multimedia content. ",
+    price: 1050,
+    countInStock: 10,
+  },
+  {
+    name: "XPS 13 Laptop",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSLQzEqr33U0tcGUBdAzmMChMzbN7aDkPPtaUq-c1aWvmD5IhC94NA5aasHzY63MHG0MKSXsk&usqp=CAc",
+    description:
+      "Your entertainment can go with you everywhere—starting at just 2.7 pounds, the small and thin XPS 13 is ready for take-off. So perfectly sized for airplane tray tables, you won’t even notice when the passenger in front of you reclines their seat.",
+    price: 1299,
+    countInStock: 10,
+  },
+
 ];
 
 module.exports = products;
