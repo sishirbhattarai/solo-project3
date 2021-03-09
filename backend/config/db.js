53a95b0a689cd25c8a32fd095650976ba02b8f81
Sishir <sishir.bhattarai@outlook.com>
2021-03-08 19:13:53 -0700

    tried changing the path on db.js to get deployment working

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/product/',
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/product',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
